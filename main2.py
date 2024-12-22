a9c73b707bb4de2a976cf2a1534a546bcc3facdf
ilgiz091 <ilgiz091@mail.ru>
2024-12-22 11:20:55 +0300

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,25 @@
+# Задание 1
+
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+print(date, task)
+
+# Задание 2
+
+date1 = input('Введите дату: ')
+task1 = input('Введите задачу: ')
+date2 = input('Введите дату: ')
+task2 = input('Введите задачу: ')
+date3 = input('Введите дату: ')
+task3 = input('Введите задачу: ')
+print(date1, task1, date2, task2, date3, task3)
+
+# Задание 3
+
+date1 = input('Введите дату: ')
+task1 = input('Введите задачу: ')
+date2 = input('Введите дату: ')
+task2 = input('Введите задачу: ')
+date3 = input('Введите дату: ')
+task3 = input('Введите задачу: ')
+dictionary = {date1: task1, date2: task2, date3: task3}
